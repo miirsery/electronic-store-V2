7d1e7a6e6853659c9600da4c89ee2a4a9594f778
miirsery <miirsery.main@gmail.com>
2022-02-09 10:34:46 +0700

    add sign in from & sign up form  & favorite icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -20,21 +20,136 @@
             <a href="/" class="actions__link">Shop</a>
           </li>
           <li class="actions__item ml-4">
-            <a href="/" class="actions__link">Favorite</a>
-          </li>
-          <li class="actions__item ml-4">
-            <a href="/" class="actions__link avatar">
-              <img src="../assets/avatar.png" alt="logo">
+            <a href="/" class="actions__link icon">
+              <img :src="imgUrl" alt="favorite">
             </a>
           </li>
+          <li class="actions__item ml-4">
+            <button
+              type="button"
+              class="actions__link icon"
+              data-micromodal-trigger="modal-1"
+              @click="toggle = !toggle"
+            >
+              <img src="../assets/avatar.png" alt="logo">
+            </button>
+          </li>
         </ul>
+        <div class="modal fixed absolute bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" v-show="toggle">
+          <div class="modal__top mb-3">
+            <button :class="{active: login}" type="button" @click="login = !login" :disabled="login">Вход /</button>
+            <button :class="{active: !login}" type="button" @click="login = !login" :disabled="!login"> Регистрация
+            </button>
+          </div>
+          <div class="w-full max-w-xs sign-in" v-show="login">
+            <form>
+              <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                  Username
+                </label>
+                <input
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="username" type="text" placeholder="Username">
+              </div>
+              <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                  Password
+                </label>
+                <input
+                  class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password" type="password" placeholder="******************">
+                <p class="text-red-500 text-xs italic">
+                  Please choose a password.
+                </p>
+              </div>
+              <div class="flex items-center justify-between">
+                <button
+                  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="button">
+                  Sign In
+                </button>
+                <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                  Forgot Password?
+                </a>
+              </div>
+            </form>
+            <p class="text-center text-gray-500 text-xs">
+              &copy;2020 Acme Corp. All rights reserved.
+            </p>
+          </div>
+          <div class="w-full max-w-xs sign-in" v-show="!login">
+            <form>
+              <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="usernameSignUp">
+                  Username
+                </label>
+                <input
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="usernameSignUp" type="text" placeholder="Username" required>
+              </div>
+              <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                  Email
+                </label>
+                <input
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email" type="text" placeholder="Email" required>
+              </div>
+              <div class="mb-4">
+                <label
+                  class="block text-gray-700 text-sm font-bold mb-2"
+                  for="passwordSignUp"
+                >
+                  Password
+                </label>
+                <input
+                  class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="passwordSignUp" type="password" placeholder="******************" required>
+                <p class="text-red-500 text-xs italic">
+                  Please choose a password.
+                </p>
+              </div>
+              <div class="mb-4">
+                <label
+                  class="block text-gray-700 text-sm font-bold mb-2"
+                  for="passwordRetry"
+                >
+                  Повторите пароль
+                </label>
+                <input
+                  class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="passwordRetry" type="password" placeholder="******************" required>
+                <p class="text-red-500 text-xs italic">
+                  Please choose a password.
+                </p>
+              </div>
+              <button
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button">
+                Sign Up
+              </button>
+            </form>
+            <p class="text-center text-gray-500 text-xs">
+              &copy;2020 Acme Corp. All rights reserved.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
-export default {};
+
+export default {
+  data() {
+    return {
+      toggle: false,
+      login: true,
+      imgUrl: require("../assets/heart.svg"),
+    };
+  }
+};
 </script>
 
 <style scoped lang="sass">
@@ -44,11 +159,34 @@ export default {};
 .wrapper
   width: 100%
 
-.avatar
+.icon
   max-width: 36px
   max-height: 36px
   display: block
+
   img
     width: 100%
     object-fit: cover
+
+
+.sign-in,
+.sign-up
+  display: flex
+  flex-direction: column
+
+.modal
+  left: 50%
+  top: 50%
+  transform: translate(-50%, -50%)
+
+  &__top
+    button
+      font-size: 22px
+      font-weight: 300
+      opacity: 0.4
+
+      &.active
+        font-weight: 500
+        opacity: 1
+
 </style>
