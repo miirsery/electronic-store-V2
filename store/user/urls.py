297722a7787965adcb3d794c843455b88f399de4
Cxatek18 <griga2888@mail.ru>
2022-03-02 16:34:59 +0500

    add registration user and avtorizatin

---
+++ store/user/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+from rest_framework_jwt.views import obtain_jwt_token
+
+router = routers.DefaultRouter()
+router.register(r'user', views.UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    
+    # По вот этому происходит авторизация
+    path('token-create/', obtain_jwt_token, name='obtain_jwt_token'),
+]
