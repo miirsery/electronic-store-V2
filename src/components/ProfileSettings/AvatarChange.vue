89e6c7a5d08a237d0e206eadeea7bb02cb34fe5e
miirsery <miirsery.main@gmail.com>
2022-03-07 22:43:22 +0700

    added a modal window for image uploading

--- src/components/ProfileSettings/AvatarChange.vue
+++ src/components/ProfileSettings/AvatarChange.vue
@@ -3,10 +3,13 @@
     <div class="menu__wrapper" v-if="showModal">
       <div class="menu__w cursor-pointer" @click="$emit('close')"></div>
       <div class="menu__content absolute left-0">
-        <div class="mb-4">
-          <button type="button">Upload a photo...</button>
+        <div class="menu__content-item">
+          <label class="file">
+            <input type="file" id="file" aria-label="File browser example">
+            <span class="block file-custom">Upload file...</span>
+          </label>
         </div>
-        <form>
+        <form class="menu__content-item">
           <button class="block" type="button">
             Remove photo
           </button>
@@ -32,10 +35,17 @@ export default {
     bottom: 0
 
   &__content
-    bottom: -4rem
+    bottom: -5.5rem
     width: 150px
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33)
-    padding: 8px 12px
+    z-index: 120
+
+    &-item
+      padding: 8px 12px
+
+      &:hover
+        background-color: #dbdbdb
+
   &__w
     position: fixed
     top: 0
@@ -48,5 +58,24 @@ export default {
     content: ""
     background: transparent
 
+.file
+  position: relative
+  cursor: pointer
+
+  input
+    opacity: 0
+    margin: 0
+
+  &-custom
+    position: absolute
+    top: 0
+    right: 0
+    left: 0
+    z-index: 5
+    width: 126px
+    color: #000
+    background-color: transparent
+    border-radius: 0.25rem
+    user-select: none
 
 </style>
