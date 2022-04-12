31a217d413b5156cecd40f4dd91915df7fa0a717
bookslomot <alexgol2101@gmail.com>
2022-04-12 20:52:33 +0400

    change user permission

--- user/permissions.py
+++ user/permissions.py
@@ -6,4 +6,4 @@ class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return bool(
             request.user and
-            request.user.is_authenticated and obj.owner.user == request.user)
+            request.user.is_authenticated and obj == request.user)
