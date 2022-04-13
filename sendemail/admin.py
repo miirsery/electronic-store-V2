ae9d5fd5f3a50fc545af422518aae7fee3e7fd9f
Alex Golub <102504564+bookslomot@users.noreply.github.com>
2022-04-13 21:40:10 +0400

    Update admin.py

--- sendemail/admin.py
+++ sendemail/admin.py
@@ -11,17 +11,6 @@ class EmailAdmin(admin.ModelAdmin):
     search_fields = ('subject',)
     list_filter = ('is_send',)
 
-    def save_model(self, request, obj, form, change):
-        obj.save()
-        print(obj.users)
-        if obj.is_send == 'SEND':
-            # user_list = []
-            # print(obj.users.all())
-            # for user in obj.users.all():
-            #     user_list.append(user.email)
-
-            send_mail(str(obj.subject), str(obj.message), settings.EMAIL_HOST_USER, [obj.users.email])
-            print(obj.is_send)
-            # print(user_list)
+    
 
 
